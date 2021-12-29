8841e7599665d4fc3d6151eec1a9325b3a54217f
OleksandrTymoshenko <nomakievip@gmail.com>
2021-12-29 14:40:57 +0200

    rebuilds v1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,14 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import SimpleLightbox from '../../node_modules/simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const startContainer = document.querySelector('.gallery');
+const createEl = galleryItems.map((item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.original}" alt='Image description' title='${item.description}'" />
+</a>`);
+startContainer.insertAdjacentHTML('beforeend', createEl.join(' '));
+console.log(startContainer);
+let gallerySet = new SimpleLightbox( '.gallery a', { captionPosition: "bottom", captionDelay: 250 } );
+gallerySet.on( 'show.simplelightbox', function () {
+  return;
+} );
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require("lodash.throttle");
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+autoLookText();
+let formData = {}
+form.addEventListener('input', throttle(slowTextTime, 1000));
+function slowTextTime() {
+    formData = {
+        email: input.value,
+        message: textArea.value
+    }
+        localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
 
+function autoLookText() {
+        const saveText = localStorage.getItem('feedback-form-state');
+        const parseText = JSON.parse(saveText);
+        if (saveText) {
+                input.value = parseText.email;
+            textArea.value = parseText.message;
+        }
+}
+
+form.addEventListener('submit', onSubmitForm)
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    const inputEmail = e.currentTarget.elements.email.value;
+    const areaText = textArea.value;
+    const obj = {
+        email: inputEmail,
+        message: areaText,
+    }
+        console.log(obj);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+}
